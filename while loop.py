87e8e327bb6149adff6b056ba8e5806238e4ad4a
panugantiguptha <158723450+panugantiguptha@users.noreply.github.com>
2026-07-10 14:42:01 +0530

    Add files via upload
    today i have learned while loop and range

---
+++ while loop.py
@@ -0,0 +1,106 @@
+#while loop()
+#it is a continuous iteration
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a<1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a-=1'''
+
+'''a=20
+while a>3:
+    print(a)
+    a=a-1'''
+
+'''a=40
+while a>5:
+    print(a)
+    a-=1'''
+
+#in the above one if we give big number and if it is greater than a then it should be "-"
+
+'''a=30
+while a>1:
+    print(a)
+    a-=1'''
+
+'''a=10
+while a>2:
+    print(a)
+    a-=1'''
+
+
+'''a=30
+while a>1:
+    print(a)
+    a-=1'''
+
+'''a=1
+while a<25:
+    print(a)
+    a+=1'''
+
+#voting
+'''while True:
+    age=int(input("enter the age "))
+    if age>=18:
+        print("eligible for vote")
+    else:
+        print("not eligible")'''
+
+#even or odd
+'''while True:
+    a=int(input("enter the number"))
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")'''
+
+
+#range()
+#the range function returns a sequence of numbers starting from 0 by default and increments by one by one and stops before a specify number.
+#start-stop-step
+#stop
+'''for i in range(20):
+    print(i)'''
+
+#start-stop
+'''for i in range (13,35):
+    print(i)'''
+
+#step
+'''for i in range(0,30,3)
+    print(i)'''
+'''for i in range(5,50,5):
+    print(i)'''
+'''for i in range(2,20,2):
+    print(i)'''
+
+#Task on all the concepts
+#in the above one we need to use condition means while beacause if we use only for loop it will print 10 times.
+#grades
+'''while True:
+    marks=int(input("enter the numer"))
+    if marks in range(91,101):
+        print("Grade A")
+    elif marks in range(81,91):
+        print("Grade B")
+    elif marks in range(70,81):
+        print("Grade C")
+    elif marks in range(50,71):
+        print("Grade D")
+    else:
+        print("Fail")'''
+
+
