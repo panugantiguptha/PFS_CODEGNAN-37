9d860a913309af591304b231310f3169eb5487f0
panugantiguptha <158723450+panugantiguptha@users.noreply.github.com>
2026-08-09 11:08:01 +0530

    Add files via upload
    in day 31 oops concepts i have learned

---
+++ OOPS.py
@@ -0,0 +1,180 @@
+#oops
+#syntax
+'''class classname():
+    #attributes
+    name="trinadh"
+    age=22
+    place="ong"
+    def fname(method_name):
+        print("statements.......")
+a=classname()
+a.fname()'''
+
+#class declaration
+'''class Details():
+    name="trinadh"
+    age=22
+    place="ong"
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.display()'''
+
+#object instatination
+'''class Details():
+    def data(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.data("trinadh",22,"ong")
+a.display()
+b=Details()
+b.data("harsha",22,"vja")
+b.display()'''
+
+#object initialization
+'''class Details():
+    #creating a constructor
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details("satish",50,"ong")
+print(dir(a))
+a.display()'''
+
+#object runtime
+'''class Details():
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+name=input("Enter the name:")
+age=int(input("Enter the age:"))
+place=input("Enter the place:")
+a=Details(name,age,place)
+print(dir(a))
+a.display()'''
+
+#Another method 
+
+'''class Details():
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details(input("name:"),int(input("age:")),input("place:"))
+print(dir(a))
+a.display()'''
+
+#Another method e
+#oops
+#syntax
+'''class classname():
+    #attributes
+    name="trinadh"
+    age=22
+    place="ong"
+    def fname(method_name):
+        print("statements.......")
+a=classname()
+a.fname()'''
+
+#class declaration
+'''class Details():
+    name="trinadh"
+    age=22
+    place="ong"
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.display()'''
+
+#object instatination
+'''class Details():
+    def data(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.data("trinadh",22,"ong")
+a.display()
+b=Details()
+b.data("harsha",22,"vja")
+b.display()'''
+
+#object initialization
+'''class Details():
+    #creating a constructor
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details("satish",50,"ong")
+print(dir(a))
+a.display()'''
+
+#object runtime
+'''class Details():
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+name=input("Enter the name:")
+age=int(input("Enter the age:"))
+place=input("Enter the place:")
+a=Details(name,age,place)
+print(dir(a))
+a.display()'''
+
+#Another method
+
+'''class Details():
+    def __init__(self,name,age,place):
+        self.name=input("name:")
+        self.age=int(input("age:"))
+        self.place=input("place:")
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details(input("name:"),int(input("age:")),input("place:"))
+print(dir(a))
+a.display()'''
+
+#Another method 
+
+'''class Details():
+    def __init__(self,name,age,place):
+        self.name=input("name:")
+        self.age=int(input("age:"))
+        self.place=input("place:")
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.display()'''
+
+
+
+
+
+
+
